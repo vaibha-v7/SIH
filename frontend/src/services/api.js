37b4db2ddbfb727eee64e052dc949058c781b13a
vaibha-v7 <vaibhav00070@gmail.com>
2025-09-15 19:37:31 +0530

    frontend moded

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://sih-besy.onrender.com/';
 
 // Quiz API functions
 export const quizAPI = {
