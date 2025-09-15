fcab04a50b805e5741b2874daecac71538a3bde2
vaibha-v7 <vaibhav00070@gmail.com>
2025-09-15 23:33:51 +0530

    gg

--- Backend/app.js
+++ Backend/app.js
@@ -23,6 +23,8 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+
   }
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
