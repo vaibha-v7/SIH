9a90716ab46eb743e178192ce2c1f1265675944f
vaibha-v7 <vaibhav00070@gmail.com>
2025-09-15 20:05:33 +0530

    gg

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://sih-besy.onrender.com/';
+const API_BASE_URL = 'https://sih-besy.onrender.com';
 
 // Quiz API functions
 export const quizAPI = {
